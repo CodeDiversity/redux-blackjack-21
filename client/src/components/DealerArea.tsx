f9b665bd81ae803b1a37275a9750dc07b38f144b
Michael Trew <40902786+CodeDiversity@users.noreply.github.com>
2026-06-14 15:32:28 -0400

    style(client): polish DealerArea with centered hand and label

--- client/src/components/DealerArea.tsx
+++ client/src/components/DealerArea.tsx
@@ -1,14 +1,28 @@
 import { useSelector } from 'react-redux';
+import styled from 'styled-components';
 import { HandView } from './HandView';
 import type { RootState } from '../store';
+
+const Wrapper = styled.div`
+  text-align: center;
+  margin-bottom: ${({ theme }) => theme.spacing.xl};
+`;
+
+const DealerLabel = styled.div`
+  font-size: ${({ theme }) => theme.typography.smallSize};
+  letter-spacing: 3px;
+  text-transform: uppercase;
+  color: ${({ theme }) => theme.colors.textSecondary};
+  margin-bottom: ${({ theme }) => theme.spacing.sm};
+`;
 
 export function DealerArea() {
   const dealer = useSelector((s: RootState) => s.game.state?.dealer);
   if (!dealer) return null;
   return (
-    <div className="dealer-area">
-      <h2>Dealer</h2>
-      <HandView hand={dealer} />
-    </div>
+    <Wrapper>
+      <DealerLabel>Dealer</DealerLabel>
+      <HandView hand={dealer} isDealer />
+    </Wrapper>
   );
 }
