56df411f6c985fdae6af091452d1a223b7db3c7f
Michael Trew <mtrew2015@gmail.com>
2026-06-23 12:41:29 -0400

    feat(server): wire PlayerModule and bootstrap DB on app boot

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,15 @@
-import { Module } from '@nestjs/common';
+import { Module, OnModuleInit } from '@nestjs/common';
 import { GameModule } from './game/game.module';
 import { RoomModule } from './room/room.module';
 import { GatewayModule } from './gateway/gateway.module';
+import { PlayerModule } from './player/player.module';
+import { initDb } from './storage/db';
 
 @Module({
-  imports: [GameModule, RoomModule, GatewayModule],
+  imports: [GameModule, RoomModule, GatewayModule, PlayerModule],
 })
-export class AppModule {}
+export class AppModule implements OnModuleInit {
+  onModuleInit() {
+    initDb();
+  }
+}
