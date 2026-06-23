72592e6c7416b3a28f64c9935d985c62df1c45fd
Michael Trew <mtrew2015@gmail.com>
2026-06-23 12:58:40 -0400

    feat(client): pass playerId in socket auth handshake

--- client/src/socket/client.ts
+++ client/src/socket/client.ts
@@ -1,10 +1,15 @@
 import { io, type Socket } from 'socket.io-client';
+import { getOrCreatePlayerId } from '../lib/player-id';
 
 let socket: Socket | null = null;
 
 export function connect(): Socket {
   if (socket) return socket;
-  socket = io('http://localhost:3001', { autoConnect: true, transports: ['websocket'] });
+  socket = io('http://localhost:3001', {
+    autoConnect: true,
+    transports: ['websocket'],
+    auth: { playerId: getOrCreatePlayerId() },
+  });
   return socket;
 }
 
